77eaf22abd440fae2b9ae127b3b5071908a7b9cf
Luan Luís <luanvwittchind@gmail.com>
2026-09-08 10:04:46 -0300

    atualizações no settings

--- config/settings.py
+++ config/settings.py
@@ -174,11 +174,11 @@ CORS_ALLOWED_ORIGINS = [
         "http://localhost:5173,"
         "http://127.0.0.1:5173,"
         "http://localhost:8080,"
-        "http://127.0.0.1:8080",
+        "http://127.0.0.1:8080,"
+        "http://localhost:8081",
     ).split(",")
     if o.strip()
 ]
-
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://lv-finance-.*\.vercel\.app$",
 ]
